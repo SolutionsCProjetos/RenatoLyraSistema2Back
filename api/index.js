d503e4ad826fcf7feaf68c6dde23d4c29884d2d2
SolutionsCProjetos <SolutionsCProjetos@hotmail.com>
2025-08-29 15:58:10 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,10 +6,13 @@
 // api/index.js
 const app = require("../src/server");
 
-// Exporta como função (garante compatibilidade do @vercel/node)
-module.exports = (req, res) => app(req, res);
+module.exports = (req, res) => {
+  return app(req, res);
+};
 
-// Opcional: evita o bodyParser da Vercel mexer no stream antes do Express
 module.exports.config = {
-  api: { bodyParser: false },
+  api: { 
+    bodyParser: false,
+    externalResolver: true 
+  },
 };
