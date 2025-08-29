a5eeda08d9fa3ed5482558eb9d8031a99b58a3d6
SolutionsCProjetos <SolutionsCProjetos@hotmail.com>
2025-08-29 15:35:37 -0300

    Update [...all].js

--- api/[...all].js
+++ api/[...all].js
@@ -1,11 +1,45 @@
-// api/[...all].js  (opção alternativa)
-const app = require('../src/server');
+// // api/[...all].js  (opção alternativa)
+// const app = require('../src/server');
+// module.exports = (req, res) => {
+//   req.url = req.url.replace(/^\/api/, '');
+//   return app(req, res);
+// };
+
+
+
+
+
+// api/[...all].js
+const app = require("../src/server");
+
+// CORS na borda (antes do Express)
+function setCors(res, origin) {
+  if (origin) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Vary", "Origin");
+  } else {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+  res.setHeader("Access-Control-Expose-Headers", "Authorization");
+}
+
 module.exports = (req, res) => {
-  req.url = req.url.replace(/^\/api/, '');
+  setCors(res, req.headers.origin);
+
+  // responde preflight aqui mesmo
+  if (req.method === "OPTIONS") {
+    res.statusCode = 204;
+    return res.end();
+  }
+
+  // ⚠️ NÃO remova o /api – deixe o path intacto
+  // req.url = req.url.replace(/^\/api/, '');
+
   return app(req, res);
 };
 
-
-
-
+// evita bodyParser da Vercel interferir
+module.exports.config = { api: { bodyParser: false } };
 
