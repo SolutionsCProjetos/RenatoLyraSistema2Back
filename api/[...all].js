f3c3a18e04eddc877ae04a16b5ed0fbffdce581d
SolutionsCProjetos <SolutionsCProjetos@hotmail.com>
2025-08-29 15:57:42 -0300

    Update [...all].js

--- api/[...all].js
+++ api/[...all].js
@@ -6,53 +6,21 @@
 // };
 
 
-
-// api/[...all].js
 const app = require("../src/server");
 
-function setCors(req, res) {
-  const origin = req.headers.origin;
-  if (origin) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Vary", "Origin");
-  } else {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-  }
-
-  // Ecoa os métodos/headers que o navegador solicitou no preflight
-  const reqMethod = req.headers["access-control-request-method"];
-  const reqHeaders = req.headers["access-control-request-headers"];
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    reqMethod ? `${reqMethod},OPTIONS` : "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    reqHeaders || "Authorization, Content-Type"
-  );
-
-  res.setHeader("Access-Control-Expose-Headers", "Authorization");
-  // opcional: cache do preflight
-  res.setHeader("Access-Control-Max-Age", "600");
-}
-
-module.exports = (req, res) => {
-  setCors(req, res);
-
-  // Responde o preflight aqui mesmo
-  if (req.method === "OPTIONS") {
-    res.statusCode = 200;       // 200 evita algum proxy que poda headers no 204
-    res.setHeader("Content-Length", "0");
-    return res.end();
-  }
-
-  // NÃO mexe no path (mantemos /api/...)
+module.exports = async (req, res) => {
+  // Remove completamente a lógica de CORS personalizada
+  // Deixa o Express lidar com CORS através do middleware cors()
   return app(req, res);
 };
 
-module.exports.config = { api: { bodyParser: false } };
+module.exports.config = { 
+  api: { 
+    bodyParser: false,
+    externalResolver: true 
+  } 
+};
+
 
 
 
